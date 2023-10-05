e14e443e4ea6e700cd3acf9c2bde926f99a39854
k21082681 <shahad.alhamazani@kcl.ac.uk>
2023-10-05 23:01:21 +0100

    First attempt

--- primes.py
+++ primes.py
@@ -3,4 +3,25 @@
 
 def primes(number_of_primes):
     list = []
+    if number_of_primes < 1:
+        raise ValueError(f'Number of primes = {number_of_primes} should have been 1 or more.')
+    list.append(2)  
+    num = 3
+    while len(list) < number_of_primes:
+        isPrime = False
+        for i in range(2, num):
+            if num % i == 0:
+                isPrime = False
+                break
+            else:
+                isPrime = True
+        if isPrime:
+            list.append(num)
+        num += 1 
+        
+         
+        
+
     return list
+
+
